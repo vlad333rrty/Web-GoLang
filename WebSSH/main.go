f5d55c3e232ed449527b60d06395c52899147197
vlad333rrty <vlad333rrty90@mail.ru>
2019-12-11 23:00:08 +0300

    reading from server improved,refactor

--- WebSSH/main.go
+++ WebSSH/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"golang.org/x/crypto/ssh"
 	"html/template"
@@ -9,13 +8,14 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strings"
 )
 
-func handleError(err error,priority bool)bool{
-	if err!=nil{
-		if priority{
+func handleError(err error, priority bool) bool {
+	if err != nil {
+		if priority {
 			panic(err)
-		}else{
+		} else {
 			log.Println(err)
 			return false
 		}
@@ -23,31 +23,38 @@ func handleError(err error,priority bool)bool{
 	return true
 }
 
-type Lore []string
+type Lore struct {
+	Lore []string
+	Dir  string
+}
 
-func main(){
-	var(
-		stdin io.WriteCloser
-		stdout io.Reader
-		out=make([]byte,65536)
-		lore=Lore{}
+func main() {
+	var (
+		stdin   io.WriteCloser
+		stdout  io.Reader
+		out     = make([]byte, 65536)
+		lore    Lore
+		session *ssh.Session
 	)
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		http.ServeFile(writer,request,"index.html")
+		http.ServeFile(writer, request, "index.html")
 	})
 	http.HandleFunc("/SSHAuthentication/result", func(writer http.ResponseWriter, request *http.Request) {
-		var ip,password,port,login string
-		flag.StringVar(&ip,"ip",request.FormValue("ip"),"server ip")
-		flag.StringVar(&password,"password",request.FormValue("password"),"user's password")
-		flag.StringVar(&port,"port",request.FormValue("port"),"port")
-		flag.StringVar(&login,"login",request.FormValue("login"),"user's login")
-		flag.Parse()
+		ip := request.FormValue("ip")
+		password := request.FormValue("password")
+		port := request.FormValue("port")
+		login := request.FormValue("login")
+
+		lore = Lore{
+			Lore: make([]string, 0),
+			Dir:  "",
+		}
 
 		var err error
-		config:=&ssh.ClientConfig{
-			Config:            ssh.Config{},
-			User:              login,
-			Auth:              []ssh.AuthMethod{
+		config := &ssh.ClientConfig{
+			Config: ssh.Config{},
+			User:   login,
+			Auth: []ssh.AuthMethod{
 				ssh.Password(password),
 			},
 			HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
@@ -55,44 +62,86 @@ func main(){
 			},
 		}
 
-		conn,err:=ssh.Dial("tcp",fmt.Sprintf("%s:%s",ip,port),config)
-		handleError(err,true)
+		conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%s", ip, port), config)
+		handleError(err, true)
 
-		session,err:=conn.NewSession()
-		handleError(err,false)
+		session, err = conn.NewSession()
+		handleError(err, false)
 
-		stdin,err=session.StdinPipe()
-		handleError(err,true)
+		modes := ssh.TerminalModes{
+			ssh.ECHO:          0,
+			ssh.TTY_OP_ISPEED: 14400,
+			ssh.TTY_OP_OSPEED: 14400,
+		}
 
-		stdout,err=session.StdoutPipe()
+		if err = session.RequestPty("xterm", 80, 40, modes); err != nil {
+			_ = session.Close()
+			log.Fatal(err)
+		}
 
-		handleError(session.Shell(),true)
+		stdin, err = session.StdinPipe()
+		handleError(err, true)
 
-		http.Redirect(writer,request,"/console",http.StatusSeeOther)
+		stdout, err = session.StdoutPipe()
+
+		handleError(err, true)
+		handleError(session.Shell(), true)
+
+		//reading greetings
+		n, err := stdout.Read(out)
+		handleError(err, false)
+
+		s := string(out[:n])
+
+		if strings.LastIndex(s, "\n") == -1 {
+			lore.Dir = s
+		} else {
+			temp := strings.Split(s, "\n")
+			for i := 0; i < len(temp); i++ {
+				lore.Lore = append(lore.Lore, temp[i])
+			}
+		}
+
+		http.Redirect(writer, request, "/console", http.StatusSeeOther)
 	})
 
 	http.HandleFunc("/console", func(writer http.ResponseWriter, request *http.Request) {
-		t,_:=template.ParseFiles("console.html")
-		_=t.Execute(writer,lore)
+		n, err := stdout.Read(out)
+		handleError(err, false)
+		t, _ := template.ParseFiles("console.html")
+
+		temp := string(out[:n])
+		k := strings.LastIndex(temp, "\n")
+
+		if k == -1 {
+			lore.Dir = temp
+		} else {
+			lore.Lore = append(lore.Lore, temp[:k-1])
+			lore.Dir = temp[k:]
+		}
+		_ = t.Execute(writer, lore)
 	})
 
 	http.HandleFunc("/SSHData/parseData", func(writer http.ResponseWriter, request *http.Request) {
-		cmd:=request.FormValue("cmd")
-		_,_=fmt.Fprintf(stdin,"%s\n",cmd)
+		cmd := request.FormValue("cmd")
+		if cmd == "clear" {
+			lore.Lore = make([]string, 0)
+			cmd = ""
+		}
+		_, err := fmt.Fprintf(stdin, "%s\n", cmd)
+		handleError(err, false)
+		if cmd == "exit" {
+			err = session.Close()
+			handleError(err, true)
 
-		fmt.Println(request.Form)
-
-		n,err:=stdout.Read(out)
-
-		handleError(err,true)
-
-		lore = append(lore, string(out[:n]))
-
-		http.Redirect(writer,request,"/console",http.StatusSeeOther)
+			http.Redirect(writer, request, "/", http.StatusSeeOther)
+		} else {
+			http.Redirect(writer, request, "/console", http.StatusSeeOther)
+		}
 	})
 
-	err:=http.ListenAndServe("localhost:9000",nil)
-	if err!=nil{
+	err := http.ListenAndServe("localhost:9000", nil)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
